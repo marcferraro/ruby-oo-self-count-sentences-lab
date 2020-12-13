7573898fe9c815d47eaa9c955feb6b983108d160
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-13 14:06:35 -0600

    commit tree 3da557158e4d7597358c4f5eea7cc71498d930c4

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split.count {|word_punctuation| word_punctuation.include?(".") || word_punctuation.include?("!") || word_punctuation.include?("?")}
   end
 end
